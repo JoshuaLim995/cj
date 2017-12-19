5a823e8c2ceff50248dc3e3a43f038d46c840eca
Joshua <limshiawjong@gmail.com>
2017-12-19 19:14:35 +0800

    remove sessions

--- admin/adminPage.php
+++ admin/adminPage.php
@@ -1,6 +1,5 @@
 <?php 
-session_start();
-if(isset($_SESSION['userID']) && $_SESSION['regisType'] == "A"){
+
 	include ('../CSS.php');
 	include ('adminHeader.php');
 	include('../connDB.php');
@@ -20,16 +19,3 @@ if(isset($_SESSION['userID']) && $_SESSION['regisType'] == "A"){
 
 	</html>
 
-
-	<?php
-}else{
-	header("HTTP/1.0 404 Not Found");
-	echo "<h1>404 Not Found</h1>";
-	echo "<p>The page that you have requested could not be found.</p>";
-
-	echo "<a href= '../login.php'>";
-	echo "Please Login to open this page.";
-	echo "</a>";
-	exit();
-}
-?>
