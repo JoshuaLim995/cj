53644a158bd2526d289be74581fce91eb7d0b849
JoshuaLim995 <limshiawjong@gmail.com>
2018-12-26 11:43:27 +0800

    changed database password

--- connDB.php
+++ connDB.php
@@ -3,11 +3,8 @@ $host = "den1.mysql4.gear.host";
 
 $db_name = "goldenage";
 $username = "goldenage";
-$password = "Ee4T_z16o~vK";
+$password = "Xf5aD1gB_j2!";
 
-//$db_name = "id4035166_goldenage";
-//$username = "id4035166_joshua";
-//$password = "12345";
 
 $con = mysqli_connect($host, $username, $password, $db_name);
 if ($con === false)
